990f08105616428f66287e566c67f525e4c23f0f
mertins <mertins@gmail.com>
2016-08-23 12:35:50 -0300

    Iniciando processo de calculo da entropia

--- ID3/src/main/java/br/com/mertins/ufpel/am/id3/CalcEntropy.java
+++ ID3/src/main/java/br/com/mertins/ufpel/am/id3/CalcEntropy.java
@@ -3,6 +3,7 @@ package br.com.mertins.ufpel.am.id3;
 import br.com.mertins.ufpel.am.preparacao.Attribute;
 import br.com.mertins.ufpel.am.preparacao.Label;
 import br.com.mertins.ufpel.am.preparacao.Register;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,23 +17,39 @@ public class CalcEntropy {
     private final List<Register> registers;
     private final Set<Label> labels;
 
-    private Map<Attribute, Double> result;
+    private Set<ParcialEntropy> resultParcial = new HashSet<>();
 
-    public CalcEntropy(List<Register> registers,Set<Label> labels) {
+    public CalcEntropy(List<Register> registers, Set<Label> labels) {
         this.registers = registers;
-        this.labels=labels;
+        this.labels = labels;
     }
-    
-    public void process(){
-        registers.forEach(register->{
-            
-            register.getAttributesInstance().forEach(attributeInstance->{
-                attributeInstance.getAttribute().getAttributesInstance();
+
+    public void process() {
+        registers.forEach(register -> {
+            register.getAttributesInstance().forEach(attributeInstance -> {
+
+                ParcialEntropy parcial = new ParcialEntropy(attributeInstance.getAttribute(), register.getLabel());
+                if (resultParcial.contains(parcial)) {
+                    resultParcial.forEach(parcialEntropy -> {
+                        if (parcial.equals(parcialEntropy)) {
+                            parcialEntropy.add();
+                        }
+                    });
+                } else {
+                    parcial.add();
+                    resultParcial.add(parcial);
+                }
             });
-            System.out.printf("%d   %s  %s\n", register.getLine(), register.getAttributesInstance().get(0).toString(), register.getLabel().toString());
+            Attribute attribute = register.getAttributesInstance().get(0).getAttribute();
+
+//            System.out.printf("%d   %s  %s\n", register.getLine(), register.getAttributesInstance().get(0).toString(), register.getLabel().toString());
         });
-        labels.forEach(label->{
-            System.out.printf("\t\t%s\n",label.getValue());
+//        labels.forEach(label -> {
+//            System.out.printf("\t\t%s\n", label.getValue());
+//        });
+
+        this.resultParcial.forEach(action -> {
+            System.out.println(action.toString());
         });
     }
 
