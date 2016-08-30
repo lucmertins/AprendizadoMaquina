afe83bf1cdfd4e3beec05de0b5e7642de6bd5d44
Luciano E Mertins <mertins@gmail.com>
2016-08-30 18:54:39 -0300

    Removendo os nodos que não tinham folhas, substituindo os por folhas com a solução

--- ID3/src/main/java/br/com/mertins/ufpel/am/id3/Execute.java
+++ ID3/src/main/java/br/com/mertins/ufpel/am/id3/Execute.java
@@ -32,7 +32,7 @@ public class Execute {
             }
             System.out.println("****** Ajuste");
             List<Integer> remove = new ArrayList<>();
-//            remove.add(0);
+            remove.add(0);
 //            remove.add(1);
             sample.removeAttributesPos(remove);
             sample.defineColumnLabel(5, "yes");
