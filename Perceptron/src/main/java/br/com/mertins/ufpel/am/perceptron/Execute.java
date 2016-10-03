f0c611b7224db2d459a7a0e9a6e08fa033c68acd
mertins <mertins@gmail.com>
2016-10-03 12:16:36 -0300

    Inicializando o perceptron

---
+++ Perceptron/src/main/java/br/com/mertins/ufpel/am/perceptron/Execute.java
@@ -0,0 +1,14 @@
+package br.com.mertins.ufpel.am.perceptron;
+
+/**
+ *
+ * @author mertins
+ */
+public class Execute {
+    public static void main(String[] args) {
+        Perceptron neuronio=new Perceptron(0);
+        neuronio.addIn(0);
+        neuronio.sum();
+    }
+ 
+}
--- Perceptron/src/main/java/br/com/mertins/ufpel/am/perceptron/Perceptron.java
+++ Perceptron/src/main/java/br/com/mertins/ufpel/am/perceptron/Perceptron.java
@@ -9,13 +9,32 @@ import java.util.List;
  */
 public class Perceptron {
 
-    private final List<Sinaps> sinaps = new ArrayList<>();
+    private final List<Sinaps> sinapsList = new ArrayList<>();
+    private double bias;
+
+    public Perceptron(double bias) {
+        this.bias = bias;
+    }
+
+    public double getBias() {
+        return bias;
+    }
+
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
 
     public void addIn(double in) {
-        sinaps.add(new Sinaps(in, 1));
+        sinapsList.add(new Sinaps(in, 1));
     }
 
     public void addIn(double in, double weight) {
-        sinaps.add(new Sinaps(in, weight));
+        sinapsList.add(new Sinaps(in, weight));
+    }
+
+    double sum() {
+        double result = 1 * bias;
+        result = sinapsList.stream().map((sin) -> sin.getIn() * sin.getWeight()).reduce(result, (accumulator, _item) -> accumulator + _item);
+        return result;
     }
 }
