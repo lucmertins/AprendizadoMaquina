ceb136a5d23deed680643e3d6ef1c5cccd17658b
mertins <mertins@gmail.com>
2016-10-03 21:52:39 -0300

    Preparando parte de treinamento

---
+++ Perceptron/src/main/java/br/com/mertins/ufpel/am/perceptron/Sample.java
@@ -0,0 +1,31 @@
+package br.com.mertins.ufpel.am.perceptron;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mertins
+ */
+public class Sample {
+    private final List<Double> values=new ArrayList<>();
+    private int value;
+    
+    public void addIn(double value){
+        values.add(value);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public List<Double> getValues() {
+        return values;
+    }
+    
+    
+}
--- Perceptron/src/main/java/br/com/mertins/ufpel/am/perceptron/Training.java
+++ Perceptron/src/main/java/br/com/mertins/ufpel/am/perceptron/Training.java
@@ -5,5 +5,10 @@ package br.com.mertins.ufpel.am.perceptron;
  * @author mertins
  */
 public class Training {
-    
+
+    private int epoca;
+
+    public void withDelta(Perceptron perceptron) {
+        
+    }
 }
