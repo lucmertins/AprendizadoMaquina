1c4dc0c5e5c22aa7a921b6a0352f8d85cacab745
mertins <mertins@gmail.com>
2016-10-28 18:43:45 -0200

    perceptron estocastica mac

--- Avaliacao/src/main/java/br/com/mertins/ufpel/avaliacao/perceptron/Execute.java
+++ Avaliacao/src/main/java/br/com/mertins/ufpel/avaliacao/perceptron/Execute.java
@@ -16,14 +16,14 @@ public class Execute {
 
     public static void treinamento(SamplesParameters parameters, boolean blocbkIfBadErr, Treinamento treinamento) {
         try {
-            File fileTreinamento = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
-            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
-//            File fileTreinamento = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
-//            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+//            File fileTreinamento = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
+//            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+            File fileTreinamento = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
+            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
             ExecTreinamento exeTreino = new ExecTreinamento();
 //            exeTreino.open(parameters, fileTreinamento, fileTest, new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"});
-            exeTreino.open(parameters, fileTreinamento, fileTest, new String[]{"0","4", "5", "8"});
-            exeTreino.run(blocbkIfBadErr, 0.000000000005, 0.6, 1000, 6, treinamento, Perceptron.AlgorithmSimoid.HARD_0);
+            exeTreino.open(parameters, fileTreinamento, fileTest, new String[]{"0", "3", "5", "8"});
+            exeTreino.run(blocbkIfBadErr, 0.00000005, 0.6, 1000, 6, treinamento, Perceptron.AlgorithmSimoid.HARD_0);
         } catch (IOException ex) {
             Logger.getLogger(ExecTreinamento.class.getName()).log(Level.SEVERE, String.format("Falha ao treinar [%s]", ex.getMessage()), ex);
         }
@@ -31,10 +31,10 @@ public class Execute {
 
     public static void avaliacao(SamplesParameters parameters) {
         try {
-            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
-//            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
-            ExecuteAvaliacao aval = new ExecuteAvaliacao(null, 5);
-            aval.run(fileTest, parameters, "/home/mertins/IAPerceptron/20161020_185511/perceptron_5_7", Perceptron.AlgorithmSimoid.HARD_0);
+//            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+            ExecuteAvaliacao aval = new ExecuteAvaliacao(null, 3);
+            aval.run(fileTest, parameters, "/Users/mertins/IAPerceptron/20161027_204650/perceptron_3_5", Perceptron.AlgorithmSimoid.HARD_0);
         } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(ExecuteAvaliacao.class.getName()).log(Level.SEVERE, String.format("Falha ao avaliar testes [%s]", ex.getMessage()), ex);
         }
@@ -47,8 +47,8 @@ public class Execute {
         parameters.setPositiveValue(1);
         parameters.setFirstLineAttribute(false);
         parameters.setColumnLabel(0);
-        Execute.treinamento(parameters, true, Treinamento.DELTA);
-//        Execute.avaliacao(parameters);
+//        Execute.treinamento(parameters, true, Treinamento.ESTOCASTICO);
+        Execute.avaliacao(parameters);
     }
 
 }
