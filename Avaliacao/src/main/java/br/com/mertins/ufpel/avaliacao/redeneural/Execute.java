2623899d4e512e6767fa6d1acf03fbf62f956408
mertins <mertins@gmail.com>
2016-11-08 09:51:31 -0200

    Reconfigurando MLP

--- Avaliacao/src/main/java/br/com/mertins/ufpel/avaliacao/redeneural/Execute.java
+++ Avaliacao/src/main/java/br/com/mertins/ufpel/avaliacao/redeneural/Execute.java
@@ -16,18 +16,16 @@ public class Execute {
 
     public static void treinamento(SamplesParameters parameters, boolean blocbkIfBadErr) {
         try {
-            File fileTreinamento = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
-            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
-//            File fileTreinamento = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
-//            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+//            File fileTreinamento = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
+//            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+            File fileTreinamento = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_train.csv");
+            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
             ExecTreinamento exeTreino = new ExecTreinamento();
             int numParametros = exeTreino.open(parameters, fileTreinamento, fileTest);
             MLP rede = new MLP();
             rede.createIn(numParametros);
-            rede.addHiddenLayer(48, Perceptron.AlgorithmSimoid.LOGISTIC);
+            rede.addHiddenLayer(60, Perceptron.AlgorithmSimoid.LOGISTIC);
             rede.addHiddenLayer(24, Perceptron.AlgorithmSimoid.LOGISTIC);
-            rede.addHiddenLayer(12, Perceptron.AlgorithmSimoid.LOGISTIC);
-            rede.addHiddenLayer(6, Perceptron.AlgorithmSimoid.LOGISTIC);
             rede.addOut(10, Perceptron.AlgorithmSimoid.LOGISTIC);
             rede.connect();
 //            MLP rede = MLP.deserialize("/Users/mertins/IARedeNeural/20161029_212238/MLP_100");
@@ -40,12 +38,13 @@ public class Execute {
 
     public static void avaliacao(SamplesParameters parameters) {
         try {
-            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
-//            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+//            File fileTest = new File("/home/mertins/Documentos/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
+            File fileTest = new File("/Users/mertins/Documents/UFPel/Dr/AprendizadoMaquina/mnist/mnist_test.csv");
 
             ExecuteAvaliacao aval = new ExecuteAvaliacao(null);
-            Accumulator[] acumuladores1 = aval.run(fileTest, parameters, "/home/mertins/IARedeNeural/20161030_233705/MLP_500");
-            Accumulator[] acumuladores2 = aval.run(fileTest, parameters, "/home/mertins/IARedeNeural/20161030_233705/MLP_1000");
+//            Accumulator[] acumuladores1 = aval.run(fileTest, parameters, "/home/mertins/IARedeNeural/20161105_165527/MLP_6");
+            Accumulator[] acumuladores1 = aval.run(fileTest, parameters, "/Users/mertins/IARedeNeural/20161106_174409/MLP_1000");
+//            Accumulator[] acumuladores2 = aval.run(fileTest, parameters, "/home/mertins/IARedeNeural/20161030_233705/MLP_1000");
 //            aval.run(fileTest, parameters, "/Users/mertins/IARedeNeural/20161030_233705");
 
             ConfusionMatrix confusao = new ConfusionMatrix();
